a4691b1dc8f09a04868534c022c7247429aeb390
AVan-Git <hoangkc35@gmail.com>
2023-10-27 21:43:04 +0700

    add jpa

--- Demo/demoGK2_JavaEE_Jpa_Mariadb/src/main/java/vn/edu/iuh/fit/se/model/Candidate.java
+++ Demo/demoGK2_JavaEE_Jpa_Mariadb/src/main/java/vn/edu/iuh/fit/se/model/Candidate.java
@@ -1,4 +1,86 @@
 package vn.edu.iuh.fit.se.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "candidate")
 public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "can_id")
+    private  long id;
+    @Column(name = "full_name", nullable = false)
+    private String name;
+    @Column(name = "phone", length = 15, unique = true)
+    private String phone;
+    @Column(name = "email", unique = true)
+    private String email;
+    // jpa
+
+    @OneToMany(mappedBy = "candidate", cascade = CascadeType.PERSIST)
+    private List<Experience> experiences;
+
+    @Override
+    public String toString() {
+        return "Candidate{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+//                ", experiences=" + experiences +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Experience> getExperiences() {
+        return experiences;
+    }
+
+    public void setExperiences(List<Experience> experiences) {
+        this.experiences = experiences;
+    }
+
+    public Candidate() {
+    }
+
+    public Candidate(long id, String name, String phone, String email, List<Experience> experiences) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.experiences = experiences;
+    }
 }
--- Demo/demoGK2_JavaEE_Jpa_Mariadb/src/main/java/vn/edu/iuh/fit/se/model/Experience.java
+++ Demo/demoGK2_JavaEE_Jpa_Mariadb/src/main/java/vn/edu/iuh/fit/se/model/Experience.java
@@ -23,4 +23,91 @@ public class Experience {
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private ExperienceRole role;
+
+    // JPa
+    @ManyToOne
+    @JoinColumn(name = "can_id",nullable = false )
+    private Candidate candidate;
+
+    @Override
+    public String toString() {
+        return "Experience{" +
+                "id=" + id +
+                ", company='" + company + '\'' +
+                ", fromDate=" + fromDate +
+                ", toDate=" + toDate +
+                ", wordDescription='" + wordDescription + '\'' +
+                ", role=" + role +
+//                ", candidate=" + candidate +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(LocalDate fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public LocalDate getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(LocalDate toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getWordDescription() {
+        return wordDescription;
+    }
+
+    public void setWordDescription(String wordDescription) {
+        this.wordDescription = wordDescription;
+    }
+
+    public ExperienceRole getRole() {
+        return role;
+    }
+
+    public void setRole(ExperienceRole role) {
+        this.role = role;
+    }
+
+    public Candidate getCandidate() {
+        return candidate;
+    }
+
+    public void setCandidate(Candidate candidate) {
+        this.candidate = candidate;
+    }
+
+    public Experience() {
+    }
+
+    public Experience(long id, String company, LocalDate fromDate, LocalDate toDate, String wordDescription, ExperienceRole role, Candidate candidate) {
+        this.id = id;
+        this.company = company;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.wordDescription = wordDescription;
+        this.role = role;
+        this.candidate = candidate;
+    }
 }
