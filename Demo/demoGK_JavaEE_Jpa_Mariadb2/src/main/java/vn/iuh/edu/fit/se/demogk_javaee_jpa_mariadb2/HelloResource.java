0ced95b1625304b2219107df0a45009459c2ada8
AVan-Git <hoangkc35@gmail.com>
2023-10-27 11:14:14 +0700

    add project new vs Rest Sevice

---
+++ Demo/demoGK_JavaEE_Jpa_Mariadb2/src/main/java/vn/iuh/edu/fit/se/demogk_javaee_jpa_mariadb2/HelloResource.java
@@ -0,0 +1,14 @@
+package vn.iuh.edu.fit.se.demogk_javaee_jpa_mariadb2;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
