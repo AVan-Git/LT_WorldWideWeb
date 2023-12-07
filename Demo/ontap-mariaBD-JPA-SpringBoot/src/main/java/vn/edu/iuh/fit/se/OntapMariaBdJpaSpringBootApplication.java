c821fdb896913d4e51303f04774ba57256b07258
AVan-Git <hoangkc35@gmail.com>
2023-12-07 16:21:47 +0700

    git start ontap spring co ban

---
+++ Demo/ontap-mariaBD-JPA-SpringBoot/src/main/java/vn/edu/iuh/fit/se/OntapMariaBdJpaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntapMariaBdJpaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntapMariaBdJpaSpringBootApplication.class, args);
+	}
+
+}
