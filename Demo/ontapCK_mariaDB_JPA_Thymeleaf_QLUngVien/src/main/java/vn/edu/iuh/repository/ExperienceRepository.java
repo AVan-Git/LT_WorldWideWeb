d63ed979fcf796b3e27012fec60ac55ae360c36b
AVan-Git <hoangkc35@gmail.com>
2023-12-13 16:51:24 +0700

    update vặt

--- Demo/ontapCK_mariaDB_JPA_Thymeleaf_QLUngVien/src/main/java/vn/edu/iuh/repository/ExperienceRepository.java
+++ Demo/ontapCK_mariaDB_JPA_Thymeleaf_QLUngVien/src/main/java/vn/edu/iuh/repository/ExperienceRepository.java
@@ -1,10 +1,12 @@
 package vn.edu.iuh.repository;
 
+import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.entity.Candidate;
 import vn.edu.iuh.entity.Experience;
 
+import java.lang.annotation.Native;
 import java.util.List;
 
 @Repository
